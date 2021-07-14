4e4f7a4552a1823fb6a3e6a2d49d18bce2301f06
Moongaze05 <makin99@yandex.ru>
2021-07-14 21:48:13 +0500

    ufffff

--- script/toggleBar.js
+++ script/toggleBar.js
@@ -3,14 +3,6 @@
  */
 
 function toggleBar() {
-    // let buttons = document.querySelectorAll('.ctrl-nav')
-    // buttons.forEach(element => {
-    //     element.classList.toggle('ctrl-toggle-off');
-    //     element.classList.toggle('ctrl-mobile');
-    //     element.classList.toggle('ctrl-toggle-on');
-    // });
-    // let buttonToggle = document.getElementById('toggle-bar');
-    // buttonToggle.classList.toggle('toggle-bar-add');
     let buttons = document.querySelector(".visible");
     buttons.classList.toggle('ctrl-toggle-off');
     buttons.classList.toggle('ctrl-toggle-on');
