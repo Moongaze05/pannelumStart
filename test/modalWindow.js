f176fb20563e0026c46e8cc297d4f207dcbf1324
Moongaze05 <makin99@yandex.ru>
2021-07-19 19:25:47 +0500

    fixModalWindowStrDash

--- test/modalWindow.js
+++ test/modalWindow.js
@@ -3,10 +3,10 @@ export let modalWindowStr = `<div class="modalwindow" id=modalwindow>
 <div class="data-input">
     <h3>Выберите тип хоспота:</h3>
     <div class="data-radio">
-        <label for="info">Информация</label>
         <input type="radio" name="hotspot" id="info">
-        <label for="scene">Сцена</label>
+        <label for="info">Информация</label>
         <input type="radio" name="hotspot" id="scene">
+        <label for="scene">Сцена</label>
     </div>
     <button onclick="makeNewHotSpot(),cross('modalwindow')">Сохранить</button>
     <button onclick="deleteNewHotSpot(),cross('modalwindow')">Удалить</button>
