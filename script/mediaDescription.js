b6c4833f2fafc8089c84cb6436cb91ae3ed9f3cc
Moongaze05 <makin99@yandex.ru>
2021-07-25 19:20:14 +0500

    wordBreakHotSpotStyle

--- script/mediaDescription.js
+++ script/mediaDescription.js
@@ -20,7 +20,7 @@ export let description =
                     <h4>${authorPic}</h4>
                 </div>
                 <div class="media-description-text-left-p">
-                    <p>Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus adipisci aspernatur aut culpa deleniti dicta dignissimos dolore dolorem earum est excepturi ipsa laboriosam maiores maxime minus natus nobis nulla obcaecati officiis placeat quo rem, sapiente tenetur ullam vel voluptates. adipisicing elit. Consequuntur cum cupiditate enim esse laudantium minima praesentium quae quasi voluptas voluptatum?</p>
+                    <p style="word-break: break-word;">Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus adipisci aspernatur aut culpa deleniti dicta dignissimos dolore dolorem earum est excepturi ipsa laboriosam maiores maxime minus natus nobis nulla obcaecati officiis placeat quo rem, sapiente tenetur ullam vel voluptates. adipisicing elit. Consequuntur cum cupiditate enim esse laudantium minima praesentium quae quasi voluptas voluptatum?</p>
                 </div>
             </div>
             <div class="media-description-text-toggler" id="toggler-text" onclick="toggleText('toggle-text')">&#9650;</div>
@@ -31,13 +31,14 @@ export let description =
             <h4>${authorPic}</h4>
         </div>
         <div class="media-picture">
-<!--        <button style="width: 10px;-->
-<!--                       height: 20px;-->
-<!--                       position: absolute;-->
-<!--                        top: 20%;"-->
-<!--                        onclick="insertMoreInfo()"-->
-<!--        >a-->
-<!--        </button>-->
+        <button class="media-info-additional-button" onclick="insertMoreInfo()">
+            <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 45.999 45.999" xml:space="preserve">
+                <path d="M39.264,6.736c-8.982-8.981-23.545-8.982-32.528,0c-8.982,8.982-8.981,23.545,0,32.528c8.982,8.98,23.545,8.981,32.528,0
+                C48.245,30.281,48.244,15.719,39.264,6.736z M25.999,33c0,1.657-1.343,3-3,3s-3-1.343-3-3V21c0-1.657,1.343-3,3-3s3,1.343,3,3V33z
+                M22.946,15.872c-1.728,0-2.88-1.224-2.844-2.735c-0.036-1.584,1.116-2.771,2.879-2.771c1.764,0,2.88,1.188,2.917,2.771
+                C25.897,14.648,24.746,15.872,22.946,15.872z"/>
+            </svg>
+        </button>
             <img src="${pic}" alt="${namePic} ${authorPic}" class='media-img'>
         </div>
         <div class="media-description-audio">
